354d4416f6f5ad882ee53c69c7013e6e7128037b
김 선호 <sunho.kim@iarkdata.com>
2022-05-29 15:03:41 +0900

    [제 3장] 별칭 문제

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -6,7 +6,8 @@ class DollarTest {
     @Test
     public void testEquality(){
         Dollar five = new Dollar(5);
-        int amount = five.times(2);
+        Dollar product = five;
+        int amount = product.times(2);
         assertEquals(10, amount);
         amount = five.times(3);
         assertEquals(15, amount);
